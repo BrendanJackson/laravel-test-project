5288f9baf50fb6b1c15e1900bfaf297dc7f26343
BrendanJackson <bajackson859@gmail.com>
2021-07-26 13:54:57 -0400

    update user factory and documentation

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Requires directories to be made in the public directory
 - public/uploads
 - public/uploads/watermarkedImages
 - public/uploads/watermarks
+
+
+## Factory Usage
+Add (100) new users to database 
+1. php artisan tinker
+2. User::factory()->count(100)->create(); 
+
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+        $fName = $this->faker->firstName();
+        $lName = $this->faker->lastName();
+
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => $fName.$lName,
+            'first_name' => $fName,
+            'last_name' => $lName,
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
